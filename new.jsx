a3564b65437615242936f3a9f26563d86b4cbd11
Demetri Robles <65309850+Drobles709@users.noreply.github.com>
2024-07-05 05:57:47 -0400

    New stage

---
+++ new.jsx
@@ -0,0 +1,38 @@
+
+import "./create.css";
+
+function App() {
+	
+  return (
+    <div className='App'>
+
+    <h1>
+      <span className = "G">G</span> 
+      <span className = "O">o</span>
+      <span className = "oz">o</span>
+      <span className = "gz">g</span>
+      <span className = "L">l</span>
+      <span className = "E">e</span>
+      
+      <span className = "I">I</span> 
+      <span className = "S">S</span>
+
+ 
+      <span className = "C">C</span>
+      <span className = "oss">o</span>
+      <span className = "L">o</span>
+      <span className = "E">l</span>
+  </h1>
+  <h2 className = "name">By Demetri Robles</h2>
+   
+
+      
+
+
+			
+			</div>
+		
+	);
+}
+
+export default App;
